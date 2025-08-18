10bfa9e254ff28a82914a895316bb1c9b8621562
Jeremy Wuille <jwuille@f6r3s14.clusters.42paris.fr>
2025-08-18 19:06:37 +0200

    add start_simulation

--- src/main.c
+++ src/main.c
@@ -6,41 +6,17 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 23:15:35 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/16 23:16:28 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/18 15:06:12 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// void	philo_routine1()
-// {
-// }
-void	phi_init(t_phi *phi, char **av)
-{
-	struct timeval	time;
-
-	phi->number_of_philosophers = atoi(av[1]);
-	gettimeofday(&time, NULL);
-	phi->time_to_start = time.tv_usec;
-	phi->time_to_die = av[2];
-	phi->time_to_eat = av[3];
-	phi->time_to_sleep = av[4];
-	if (av[5])
-		phi->number_of_times_each_philo_must_eat = atoi(av[5]);
-	else
-		phi->number_of_times_each_philo_must_eat = -1;
-	if (phi->number_of_times_each_philo_must_eat > EAT_LIMIT
-		|| phi->number_of_times_each_philo_must_eat < -1)
-		quit_error(ERR_EAT_LIMIT);
-	printf("%lu\n", phi->time_to_start);
-}
-
 int	main(int ac, char **av)
 {
-	t_phi	phi;
-
 	if (ac < 5 || ac > 6)
 		quit_error(ERR_NB_ARG);
-	phi_init(&phi, av);
+	if (start_simulation(av) < 0)
+		quit_error("");
 	return (EXIT_SUCCESS);
 }
