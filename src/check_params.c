3115744b5319467b04e0d5af1f33a3a4de62a507
Jeremy Wuille <jwuille@f6r4s9.clusters.42paris.fr>
2025-08-19 17:45:04 +0200

    (check_param): update -- rmv is_uint

--- src/check_params.c
+++ src/check_params.c
@@ -6,17 +6,11 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 16:53:22 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/18 17:42:15 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/19 17:41:44 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-static bool	is_uint(char *arg)
-{
-	(void)arg;
-	return (true);
-}
 
 static bool	is_number(char *arg)
 {
@@ -40,7 +34,7 @@ bool	check_params(char **av)
 	i = 0;
 	while (av[++i])
 	{
-		if (!is_number(av[i]) || !(is_uint(av[i])))
+		if (!is_number(av[i]))
 			return (false);
 	}
 	return (true);
