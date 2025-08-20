3e4d83270f56a2ff206aa1c1fe6216b9ebff7042
Jeremy Wuille <jwuille@f6r3s3.clusters.42paris.fr>
2025-08-20 17:09:30 +0200

    (routine): add print msg for action philosophers

--- src/routine.c
+++ src/routine.c
@@ -6,28 +6,23 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 15:59:36 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/20 16:20:18 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/20 17:05:40 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static bool	philo_think(t_param param)
+static bool	philo_sleep(t_param param, int philo)
 {
-	if (usleep(param.time_to_think) < 0)
-		return (false);
-	return (true);
-}
-
-static bool	philo_sleep(t_param param)
-{
+	print_time(param, SLEEP, philo);
 	if (usleep(param.time_to_sleep) < 0)
 		return (false);
 	return (true);
 }
 
-static bool	philo_eat(t_param param)
+static bool	philo_eat(t_param param, int philo)
 {
+	print_time(param, EAT, philo);
 	if (usleep(param.time_to_eat) < 0)
 		return (false);
 	return (true);
@@ -36,16 +31,18 @@ static bool	philo_eat(t_param param)
 void	*routine(void *data)
 {
 	int	meal_to_eat;
+	int	philo;
 
 	meal_to_eat = ((t_philosoph *)data)->param.number_of_philosophers;
+	philo = ((t_philosoph *)data)->nbr;
 	while (meal_to_eat != 0)
 	{
-		philo_eat(((t_philosoph *)data)->param);
+		philo_eat(((t_philosoph *)data)->param, philo);
 		meal_to_eat--;
 		if (meal_to_eat == 0)
 			return (NULL);
-		philo_sleep(((t_philosoph *)data)->param);
-		philo_think(((t_philosoph *)data)->param);
+		philo_sleep(((t_philosoph *)data)->param, philo);
+		print_time(((t_philosoph *)data)->param, THINK, philo);
 	}
 	return (NULL);
 }
