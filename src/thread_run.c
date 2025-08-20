5ea13f6f9f6edd147012bb3cb33400266374c513
skc <sheikouz@gmail.com>
2025-08-21 00:09:40 +0200

    (thread_run): fix -- leaks on ulimit -v 150000 ./philo 6 x x x

--- src/thread_run.c
+++ src/thread_run.c
@@ -6,7 +6,7 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 17:11:43 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/20 16:22:19 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/21 00:02:35 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,25 @@ bool	thread_run(t_philosoph *philo)
 	pthread_t	*threads;
 	int			i;
 	int			nbr;
+	bool		success;
 
+	success = true;
 	nbr = (*philo).param.number_of_philosophers;
 	threads = malloc(sizeof(pthread_t) * nbr);
 	if (!threads)
 		quit_error(ERR_MALLOC);
 	i = -1;
-	while (++i < nbr)
+	while (success && ++i < nbr)
 	{
 		if (pthread_create(&(threads[i]), NULL,
-				&routine, &(philo[i])) < 0)
-			quit_error("Error: thread_create failed\n");
+				&routine, &(philo[i])) != 0)
+			break ;
 	}
-	i = -1;
-	while (++i < nbr)
+	while (success && --i > -1)
 	{
-		if (pthread_join(threads[i], NULL) < 0)
-			quit_error("Error: thread_join failed\n");
+		if (pthread_join(threads[i], NULL) != 0)
+			break ;
 	}
 	free(threads);
-	return (true);
+	return (success);
 }
