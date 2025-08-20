9a130f29ac4aa9ff845cbb9c44cea4e77e3c4dc9
Jeremy Wuille <jwuille@f6r3s3.clusters.42paris.fr>
2025-08-20 16:22:55 +0200

    (free_memory): rmv free_philos

--- src/free_memory.c
+++ src/free_memory.c
@@ -6,7 +6,7 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 16:24:27 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/19 16:59:43 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/20 15:53:38 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,4 @@ void	free_forks(t_fork *fork, t_param param)
 	while (++i < param.number_of_philosophers)
 		pthread_mutex_destroy(&(fork[i].fork_lock));
 	free(fork);
-}
-
-void	free_philos(t_philosoph *philo, t_param param)
-{
-	int	i;
-
-	i = -1;
-	while (++i < param.number_of_philosophers)
-		pthread_mutex_destroy(&(philo[i].meal_eaten.meal_lock));
-	free(philo);
 }
