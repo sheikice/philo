65ce38ff84e9d23f684d39a5d78ea5f75ebbef3b
Jeremy Wuille <jwuille@f6r3s3.clusters.42paris.fr>
2025-08-20 16:26:37 +0200

    (thread_run): update -- add allocation for threads

--- src/thread_run.c
+++ src/thread_run.c
@@ -6,44 +6,35 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 17:11:43 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/19 17:37:29 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/20 16:22:19 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*routine(void *data)
-{
-	int	i;
-
-	(void) data;
-	i = -1;
-	while (++i < 100)
-		printf("philo %d is sleeping\n", ((t_philosoph *)data)->nbr);
-	return (NULL);
-}
-
-bool	thread_run(t_philosoph *philo, t_param param)
+bool	thread_run(t_philosoph *philo)
 {
 	pthread_t	*threads;
 	int			i;
+	int			nbr;
 
-	threads = malloc(sizeof(pthread_t) * param.number_of_philosophers);
+	nbr = (*philo).param.number_of_philosophers;
+	threads = malloc(sizeof(pthread_t) * nbr);
 	if (!threads)
 		quit_error(ERR_MALLOC);
 	i = -1;
-	while (++i < param.number_of_philosophers)
+	while (++i < nbr)
 	{
-		printf("run thread[%d]\n", i);
 		if (pthread_create(&(threads[i]), NULL,
 				&routine, &(philo[i])) < 0)
 			quit_error("Error: thread_create failed\n");
 	}
 	i = -1;
-	while (++i < param.number_of_philosophers)
+	while (++i < nbr)
 	{
 		if (pthread_join(threads[i], NULL) < 0)
 			quit_error("Error: thread_join failed\n");
 	}
+	free(threads);
 	return (true);
 }
