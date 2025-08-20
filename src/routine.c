65b6b99295c81bc0031abde6cf09315e61994cb8
skc <sheikouz@gmail.com>
2025-08-20 23:18:31 +0200

    (routine): fix -- meals assigning good variable

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 15:59:36 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/20 17:05:40 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/20 23:15:27 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,17 @@ static bool	philo_eat(t_param param, int philo)
 
 void	*routine(void *data)
 {
-	int	meal_to_eat;
+	int	meals;
 	int	philo;
 
-	meal_to_eat = ((t_philosoph *)data)->param.number_of_philosophers;
+	meals = ((t_philosoph *)data)->param.number_of_times_each_philo_must_eat;
 	philo = ((t_philosoph *)data)->nbr;
-	while (meal_to_eat != 0)
+	while (meals != 0)
 	{
 		philo_eat(((t_philosoph *)data)->param, philo);
-		meal_to_eat--;
-		if (meal_to_eat == 0)
-			return (NULL);
+		meals--;
+		if (meals == 0)
+			break ;
 		philo_sleep(((t_philosoph *)data)->param, philo);
 		print_time(((t_philosoph *)data)->param, THINK, philo);
 	}
