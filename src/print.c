fd27e94e0f0df2cf4c2d9b8919d19e08e55134a5
skc <sheikouz@gmail.com>
2025-08-23 19:06:28 +0200

    (print): refacto, feat -- add mutex on print

--- src/print.c
+++ src/print.c
@@ -1,42 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_time.c                                       :+:      :+:    :+:   */
+/*   print.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jwuille <jwuille@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 16:30:25 by jwuille           #+#    #+#             */
-/*   Updated: 2025/08/21 13:44:42 by jwuille          ###   ########.fr       */
+/*   Updated: 2025/08/23 18:32:15 by jwuille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-bool	print_time(t_param param, enum e_state state, int philo)
+unsigned long	time_get(void)
 {
 	struct timeval	time;
-	unsigned long	time_int;
 
-	if (gettimeofday(&time, NULL) < 0)
-		return (false);
-	time_int = time.tv_sec * 1000 + time.tv_usec / 1000 - param.time_start;
-	if (state == EAT)
-		printf("%lu ms %d has taken a fork\n", time_int, philo);
-	if (state == THINK)
-		printf("%lu ms %d is thinking\n", time_int, philo);
-	if (state == SLEEP)
-		printf("%lu ms %d is sleeping\n", time_int, philo);
-	if (state == DIE)
-		printf("%lu ms %d died\n", time_int, philo);
-	return (true);
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
-void	print_err(char *str)
+int	print_status(char *str, int nbr, pthread_mutex_t *lock)
+{
+	pthread_mutex_lock(lock);
+	printf(str, time_get(), nbr, time_get(), nbr);
+	pthread_mutex_unlock(lock);
+	return (EXIT_SUCCESS);
+}
+
+int	print_msg(char *str, int fd)
 {
 	int	i;
 
 	i = 0;
 	while (str[i])
 		i++;
-	write(2, str, i);
+	if (write(fd, str, i) < 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
